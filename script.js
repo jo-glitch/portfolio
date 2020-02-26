bf3a3c5e04495c635f9f1c83ade46d54f9706f16
Joan F <joanfoubertetude@gmail.com>
2020-02-26 21:58:33 +0100

    link done

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+// permet de faire une transition douce
+$("#animationAccueil").click(function(){
+    $("html, body").animate({scrollTop:0}, 1500);
+});
+$("#animationPropos").click(function(){
+    $("html, body").animate({scrollTop:$("#propos").offset().top}, 1500);
+});
+$("#animationProjet").click(function(){
+    $("html, body").animate({scrollTop:$("#projet").offset().top}, 1500);
+});
+$("#animationCompetences").click(function(){
+    $("html, body").animate({scrollTop:$("#competences").offset().top}, 1500);
+});
+$("#animationContact").click(function(){
+    $("html, body").animate({scrollTop:$("#contact").offset().top}, 1500);
+});
+$("#top").click(function(){
+    $("html, body").animate({scrollTop:$("#photo-cv").offset().top}, 1500);
+});
+// funtion pour cacher et faire apparaître
 $(function(){
-    $('#discord').click(function(){
-        $('#login').toggle();
-    });
- });
-
-$(function(){
+    $('#number-phone').hide();
     $('#phone').click(function(){
-       $('#number-phone').toggle()
-    });
- });
+        $('#number-phone').toggle()
+     });
+ }); 
